472d6c44de82d9ed34f0e0e242b7b0a6ef8c0032
Bryan Khor <67664138+bryankhor07@users.noreply.github.com>
2023-07-05 21:05:29 -0700

    Create Directory.java

---
+++ Directory.java
@@ -0,0 +1,25 @@
+/*
+ * **********************************************
+ * San Francisco State University
+ * CSC 220 -  Data Structures
+ * File Name: Directory.java
+ * Author: Duc Ta
+ * Author: Bryan Khor
+ * **********************************************
+ */
+
+package CSC220ASMT2;
+
+public final class Directory {
+
+    //
+    // Constructors
+    //
+    public Directory() {
+    }
+
+    public Object getPath() {
+        return "./src/assignment02PartB/log/Receipt-*-*.log";
+    }
+
+}
