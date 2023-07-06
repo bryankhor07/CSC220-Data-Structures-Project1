1da987d991d4600436d76b40ba375c9a1ddfa7e4
Bryan Khor <67664138+bryankhor07@users.noreply.github.com>
2023-07-05 21:19:51 -0700

    Create Receipt.java

---
+++ Receipt.java
@@ -0,0 +1,44 @@
+/*
+ * **********************************************
+ * San Francisco State University
+ * CSC 220 -  Data Structures
+ * File Name: Receipt.java
+ * Author: Duc Ta
+ * Author: Bryan Khor
+ * **********************************************
+ */
+
+package CSC220ASMT2;
+
+public final class Receipt {
+
+    //
+    // Static Data Fields
+    //
+
+    //
+    // Instance Data Fields
+    //
+
+    //
+    // Constructors
+    //
+    public Receipt() {
+    }
+
+//
+// Static Methods
+//
+
+//
+// Instance Methods
+//
+
+//
+// Additional Methods
+//
+
+//
+// Language
+//
+}
